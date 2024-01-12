781b5facb72dd5bb8b793d338f144f85b39de06c
CrashStark <starkcrash0@gmail.com>
2024-01-12 11:11:45 +0530

    Reverse a linked list using java

---
+++ ReverseLink.java
@@ -0,0 +1,64 @@
+class Node{
+	int data;
+	Node next;
+
+	Node(int data){
+		this.data=data;
+	}
+}
+
+class LinkedList{
+	Node head;
+	Node tail;
+
+	public void insertList(int data){
+		Node node = new Node(data);
+
+		if(head==null){
+			head=node;
+			tail=node;
+		}else{
+			tail.next=node;
+			tail=node;
+		}
+
+
+	}
+
+	public void reverseList(){
+		Node current=head;
+		Node prev=null,next=null;
+			while(current!=null){
+				next=current.next;
+				current.next=prev;
+				prev=current;
+				current=next;
+			}
+
+			head=prev;
+			travers(head);
+
+	}
+
+	public void travers(Node node){
+			Node current=node;
+
+			while(current!=null){
+				System.out.println(current.data);
+				current=current.next;
+			}
+	}
+
+}
+
+class ReverseLink{
+	public static void main(String[] args) {
+		LinkedList l1=new LinkedList();
+		l1.insertList(1);
+		l1.insertList(2);
+		l1.insertList(3);
+		l1.insertList(4);
+		l1.reverseList();
+
+	}
+}
